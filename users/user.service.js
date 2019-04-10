07f7a3448dca32bc3348d51a01e1e648b6be1e3b
Berker <demirerberker@gmail.com>
2019-04-11 00:47:37 +0100

    controller and service fixes according to new model

--- users/user.service.js
+++ users/user.service.js
@@ -6,15 +6,12 @@ const User = db.User;
 
 module.exports = {
     authenticate,
-    getAll,
     getById,
     create,
-    update,
-    delete: _delete
 };
 
 async function authenticate({ username, password }) {
-    const user = await User.findOne({ username });
+    const user = await User.findOne({ email });
     if (user && bcrypt.compareSync(password, user.hash)) {
         const { hash, ...userWithoutHash } = user.toObject();
         const token = jwt.sign({ sub: user.id }, config.secret);
@@ -25,18 +22,14 @@ async function authenticate({ username, password }) {
     }
 }
 
-async function getAll() {
-    return await User.find().select('-hash');
-}
-
 async function getById(id) {
     return await User.findById(id).select('-hash');
 }
 
 async function create(userParam) {
     // validate
-    if (await User.findOne({ username: userParam.username })) {
-        throw 'Username "' + userParam.username + '" is already taken';
+    if (await User.findOne({ username: userParam.email })) {
+        throw 'e-mail "' + userParam.email + '" is already exists';
     }
 
     const user = new User(userParam);
@@ -48,28 +41,4 @@ async function create(userParam) {
 
     // save user
     await user.save();
-}
-
-async function update(id, userParam) {
-    const user = await User.findById(id);
-
-    // validate
-    if (!user) throw 'User not found';
-    if (user.username !== userParam.username && await User.findOne({ username: userParam.username })) {
-        throw 'Username "' + userParam.username + '" is already taken';
-    }
-
-    // hash password if it was entered
-    if (userParam.password) {
-        userParam.hash = bcrypt.hashSync(userParam.password, 10);
-    }
-
-    // copy userParam properties to user
-    Object.assign(user, userParam);
-
-    await user.save();
-}
-
-async function _delete(id) {
-    await User.findByIdAndRemove(id);
 }
--- users/users.controller.js
+++ users/users.controller.js
@@ -5,11 +5,9 @@ const userService = require('./user.service');
 // routes
 router.post('/authenticate', authenticate);
 router.post('/register', register);
-router.get('/', getAll);
 router.get('/current', getCurrent);
 router.get('/:id', getById);
-router.put('/:id', update);
-router.delete('/:id', _delete);
+
 
 module.exports = router;
 
@@ -25,12 +23,6 @@ function register(req, res, next) {
         .catch(err => next(err));
 }
 
-function getAll(req, res, next) {
-    userService.getAll()
-        .then(users => res.json(users))
-        .catch(err => next(err));
-}
-
 function getCurrent(req, res, next) {
     userService.getById(req.user.sub)
         .then(user => user ? res.json(user) : res.sendStatus(404))
@@ -40,17 +32,5 @@ function getCurrent(req, res, next) {
 function getById(req, res, next) {
     userService.getById(req.params.id)
         .then(user => user ? res.json(user) : res.sendStatus(404))
-        .catch(err => next(err));
-}
-
-function update(req, res, next) {
-    userService.update(req.params.id, req.body)
-        .then(() => res.json({}))
-        .catch(err => next(err));
-}
-
-function _delete(req, res, next) {
-    userService.delete(req.params.id)
-        .then(() => res.json({}))
         .catch(err => next(err));
 }
